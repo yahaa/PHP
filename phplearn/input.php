8043752a1a6c0a79bde333adfb42942d570b9b71
yahaa <1477765176@qq.com>
2016-05-24 11:22:04 +0800

    yahaa

--- phplearn/input.php
+++ phplearn/input.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 	<body>
-<<<<<<< HEAD
 		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 			name:<input type="text" name="md5">
 			<input type="submit">
@@ -22,15 +21,6 @@
 			}
 			else echo "can`t find\n";
 			mysql_close($conn);
-=======
-		<form method="get" action="inout.php">
-			name:<input type="text" name="name">
-			date:<input type="text" name="date">
-			<input type="submit">
-		</form>
-		<?php
-			echo date("Y/M/d");
->>>>>>> 6066422332c30ca9fbc0143b9f28ab6443d8495b
 		?>
 	</body>
 </html>
