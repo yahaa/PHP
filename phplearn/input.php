c7529c436e135dac05817912e9731c1d350a23ec
yahaa <1477765176@qq.com>
2016-05-24 11:19:21 +0800

    finish find

--- phplearn/input.php
+++ phplearn/input.php
@@ -1,21 +1,26 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<form method="post" action="paoo.php">
-			name:<input type="text" name="fname">
-			date:<input type="text" name="date">
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			name:<input type="text" name="md5">
 			<input type="submit">
 		</form>
-		/*<?php
-			$sum=$_POST['fname']+$_POST['date'];
-			echo $sum;
-			echo "aklsjdklfjlkdjf";
-			echo __LINE__;
-			echo "<br>";
-			echo __FILE__;
-			echo __DIR__;
-			echo __NEMESPCE__;
-
-		?>*/
+		<?php
+			$host='localhost:3036';
+			$user='root';
+			$pass='  ';
+			$conn=mysql_connect($host,$user,$pass);
+			if(!$conn)die('false1:'.mysql_error());
+			mysql_select_db('study',$conn);
+			$md55=$_POST["md5"];
+			$sql="select pass FROM MD5_Map WHERE md5=\"$md55\"";
+			$can=mysql_query($sql,$conn);
+			if(!$can)die('false2:'.mysql_error());
+			if($ans=mysql_fetch_array($can,MYSQLI_ASSOC)){
+				echo $ans[pass];
+			}
+			else echo "can`t find\n";
+			mysql_close($conn);
+		?>
 	</body>
 </html>
